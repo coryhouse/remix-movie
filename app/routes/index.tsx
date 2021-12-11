655ca6695815e98df541c6fa98ab2ec1bab1fb17
Cory House <housecor@gmail.com>
2021-12-11 13:56:17 -0600

    Add app

---
+++ app/routes/index.tsx
@@ -0,0 +1,55 @@
+import {
+  MetaFunction,
+  useLoaderData,
+  LoaderFunction,
+  useTransition,
+  Form,
+} from "remix";
+
+type;
+
+// Loaders provide data to components and are only ever called on the server, so
+// you can connect to a database or run any server side code you want right next
+// to the component that renders it.
+// https://remix.run/api/conventions#loader
+export let loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchParam = url.searchParams.get("search");
+  const result = await fetch(
+    `http://www.omdbapi.com/?s=${searchParam}&apikey=${process.env.OMDB_API_KEY}`
+  );
+  const data = await result.json();
+  return data.Search || [];
+};
+
+// https://remix.run/api/conventions#meta
+export let meta: MetaFunction = () => {
+  return {
+    title: "Remix Movie",
+    description: "Welcome to remix Movies!",
+  };
+};
+
+// https://remix.run/guides/routing#index-routes
+export default function Index() {
+  let data = useLoaderData();
+  const transition = useTransition();
+
+  return (
+    <>
+      <Form method="get" action="/">
+        <input
+          autoComplete="off"
+          name="search"
+          type="text"
+          placeholder="Enter a movie"
+        />
+      </Form>
+      <div className="results">
+        {transition.state === "submitting"
+          ? "Submitting..."
+          : data.map((item) => <img src={item.Poster} />)}
+      </div>
+    </>
+  );
+}
