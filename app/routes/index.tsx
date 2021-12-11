b09aa536062292b924797e64d1cb6ca4db820542
Cory House <housecor@gmail.com>
2021-12-11 16:48:10 -0600

    Enhance

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,9 +4,10 @@ import {
   LoaderFunction,
   useTransition,
   Form,
+  useLocation,
 } from "remix";
 
-type MovieSearchResult = {
+type Movies = {
   imdbID: string;
   Title: string;
   Year: string;
@@ -14,50 +15,49 @@ type MovieSearchResult = {
   Poster: string;
 };
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const searchParam = url.searchParams.get("search");
   const result = await fetch(
     `http://www.omdbapi.com/?s=${searchParam}&apikey=${process.env.OMDB_API_KEY}`
   );
   const data = await result.json();
-  return data.Search || [];
+  return (data.Search || []) as Movies[];
 };
 
-// https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return {
     title: "Remix Movie",
     description: "Welcome to Remix Movies!",
   };
 };
 
-// https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let movies = useLoaderData<MovieSearchResult[]>();
+  const movies = useLoaderData<Movies[]>();
+  const { search } = useLocation();
   const transition = useTransition();
+  const searchTerm = search.split("=")[1];
 
   return (
     <>
       <Form method="get" action="/">
         <input
-          autoComplete="off"
           name="search"
-          type="text"
+          type="search"
           placeholder="Enter a movie"
+          defaultValue={searchTerm}
         />
       </Form>
-      <div className="results">
+      <section>
         {transition.state === "submitting"
-          ? "Submitting..."
+          ? "Searching..."
           : movies.map((movie) => (
               <img key={movie.imdbID} src={movie.Poster} />
             ))}
-      </div>
+        {transition.state === "idle" && movies.length === 0 && (
+          <p>No movies matching "{searchTerm}" found.</p>
+        )}
+      </section>
     </>
   );
 }
